102691ffbb51f3e45e1e43de1097e26b83ea7206
gsemir0418 <845217811@qq.com>
2022-07-14 15:39:56 +0800

    chore: 修改logo、布局等默认配置

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -2,19 +2,19 @@ import { Settings as LayoutSettings } from '@ant-design/pro-components';
 
 const Settings: LayoutSettings & {
   pwa?: boolean;
-  logo?: string;
+  logo?: boolean;
 } = {
-  navTheme: 'light',
+  navTheme: 'dark',
   // 拂晓蓝
   primaryColor: '#1890ff',
-  layout: 'mix',
+  layout: 'side',
   contentWidth: 'Fluid',
-  fixedHeader: false,
+  fixedHeader: true,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: '产品名称',
   pwa: false,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: false,
   iconfontUrl: '',
 };
 
