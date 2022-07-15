f046b406844bd156ff4e135a32f1a644e64afd9f
gsemir0418 <845217811@qq.com>
2022-07-15 14:42:39 +0800

    refactor: 封装面包屑组件（原生不支持显示一级菜单）

--- src/app.tsx
+++ src/app.tsx
@@ -1,52 +1,51 @@
-import Footer from '@/components/Footer';
-import RightContent from '@/components/RightContent';
-import { BookOutlined, LinkOutlined } from '@ant-design/icons';
-import type { Settings as LayoutSettings } from '@ant-design/pro-components';
-import { PageLoading, SettingDrawer } from '@ant-design/pro-components';
-import type { RunTimeLayoutConfig } from 'umi';
-import { history, Link } from 'umi';
-import defaultSettings from '../config/defaultSettings';
-import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
+import Footer from '@/components/Footer'
+import RightContent from '@/components/RightContent'
+import type { Settings as LayoutSettings } from '@ant-design/pro-components'
+import { PageLoading, SettingDrawer } from '@ant-design/pro-components'
+import type { RunTimeLayoutConfig } from 'umi'
+import { history } from 'umi'
+import defaultSettings from '../config/defaultSettings'
+import { currentUser as queryCurrentUser } from './services/ant-design-pro/api'
 
-const isDev = process.env.NODE_ENV === 'development';
-const loginPath = '/user/login';
+// const isDev = process.env.NODE_ENV === 'development'
+const loginPath = '/user/login'
 
 /** 获取用户信息比较慢的时候会展示一个 loading */
 export const initialStateConfig = {
   loading: <PageLoading />,
-};
+}
 
 /**
  * @see  https://umijs.org/zh-CN/plugins/plugin-initial-state
  * */
 export async function getInitialState(): Promise<{
-  settings?: Partial<LayoutSettings>;
-  currentUser?: API.CurrentUser;
-  loading?: boolean;
-  fetchUserInfo?: () => Promise<API.CurrentUser | undefined>;
+  settings?: Partial<LayoutSettings>
+  currentUser?: API.CurrentUser
+  loading?: boolean
+  fetchUserInfo?: () => Promise<API.CurrentUser | undefined>
 }> {
   const fetchUserInfo = async () => {
     try {
-      const msg = await queryCurrentUser();
-      return msg.data;
+      const msg = await queryCurrentUser()
+      return msg.data
     } catch (error) {
-      history.push(loginPath);
+      history.push(loginPath)
     }
-    return undefined;
-  };
+    return undefined
+  }
   // 如果不是登录页面，执行
   if (history.location.pathname !== loginPath) {
-    const currentUser = await fetchUserInfo();
+    const currentUser = await fetchUserInfo()
     return {
       fetchUserInfo,
       currentUser,
       settings: defaultSettings,
-    };
+    }
   }
   return {
     fetchUserInfo,
     settings: defaultSettings,
-  };
+  }
 }
 
 // ProLayout 支持的api https://procomponents.ant.design/components/layout
@@ -59,28 +58,43 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
     },
     footerRender: () => <Footer />,
     onPageChange: () => {
-      const { location } = history;
+      const { location } = history
       // 如果没有登录，重定向到 login
-      if (!initialState?.currentUser && location.pathname !== loginPath) {
-        history.push(loginPath);
-      }
+      if (!initialState?.currentUser && location.pathname !== loginPath) history.push(loginPath)
     },
-    links: isDev
-      ? [
-          <Link key="openapi" to="/umi/plugin/openapi" target="_blank">
-            <LinkOutlined />
-            <span>OpenAPI 文档</span>
-          </Link>,
-          <Link to="/~docs" key="docs">
-            <BookOutlined />
-            <span>业务组件文档</span>
-          </Link>,
-        ]
-      : [],
+    // links: isDev
+    //   ? [
+    //       <Link key="openapi" to="/umi/plugin/openapi" target="_blank">
+    //         <LinkOutlined />
+    //         <span>OpenAPI 文档</span>
+    //       </Link>,
+    //       <Link to="/~docs" key="docs">
+    //         <BookOutlined />
+    //         <span>业务组件文档</span>
+    //       </Link>,
+    //     ]
+    //   : [],
     menuHeaderRender: undefined,
     // 自定义 403 页面
     // unAccessible: <div>unAccessible</div>,
-    // 增加一个 loading 的状态
+    // 封装面包屑组件（原生不支持显示一级菜单）
+    headerContentRender: (BasicLayoutProps: any) => {
+      const menuKeys = [...BasicLayoutProps.matchMenuKeys]
+      if (!menuKeys.length) return 404
+      if (menuKeys.length > 1) {
+        // 二级菜单
+        return (
+          <>
+            <span style={{ color: '#999' }}>{BasicLayoutProps.breadcrumb[menuKeys[0]]?.name}</span>
+            <span style={{ padding: '0 8px' }}>/</span>
+            <span>{BasicLayoutProps.breadcrumb[menuKeys[1]]?.name}</span>
+          </>
+        )
+      } else {
+        // 一级菜单
+        return <span>{BasicLayoutProps.breadcrumb[menuKeys[0]]?.name}</span>
+      }
+    },
     childrenRender: (children, props) => {
       // if (initialState?.loading) return <PageLoading />;
       return (
@@ -91,17 +105,17 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
               disableUrlParams
               enableDarkTheme
               settings={initialState?.settings}
-              onSettingChange={(settings) => {
-                setInitialState((preInitialState) => ({
+              onSettingChange={settings => {
+                setInitialState(preInitialState => ({
                   ...preInitialState,
                   settings,
-                }));
+                }))
               }}
             />
           )}
         </>
-      );
+      )
     },
     ...initialState?.settings,
-  };
-};
+  }
+}
